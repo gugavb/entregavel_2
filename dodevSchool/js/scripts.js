69763abe11f22d948d085e6c96a4577b2d5ae820
Gustavo Vieira <gustavoabc2005@hotmail.com>
2025-06-04 19:49:48 -0300

    Concluindo o projeto

--- dodevSchool/js/scripts.js
+++ dodevSchool/js/scripts.js
@@ -3,32 +3,67 @@
 ////////////////////////////////////////////////////////////////////////
 
 class Aluno {
-  
+  nome
+  idade
+  nota
+
+  constructor(nome,idade,nota){
+    this.nome = nome
+    this.idade = idade
+    this.nota = nota
+  }
 }
 
 // Array
-
+let arraylunos = []
 
 //funções projeto
 
-function CadastrarAluno() {
-  
+function CadastrarAluno(nome, idade, nota, array) {
+  let objetoAluno = new Aluno(nome, idade, nota)
+  if(!array.some(x => x.nome == nome))
+    array.push(objetoAluno)
+  return objetoAluno
 }
 
-function OrdenarPorNota() {
- 
+function OrdenarPorNota(array) {
+  array.sort((a, b) => a.nota - b.nota)
+  return array
 }
 
-function OrdenarPorIdade() {
-
+function OrdenarPorIdade(array) {
+  array.sort((a, b) => a.idade - b.idade)
+  return array
 }
 
-function OrdenarPorNome() {
+function OrdenarPorNome(array) {
+  array.sort((a, b) => {
+    // Converter para maiúsculas para garantir a ordem correta
+    const nomeA = a.nome.toUpperCase();
+    const nomeB = b.nome.toUpperCase();
 
+    if(nomeA < nomeB) {
+      return -1
+    }
+    if(nomeA > nomeB) {
+      return 1
+    }
+    return 0
+  });
+  return array
 }
 
-function CalcularMedia(){
+function CalcularMedia(array){
+  if (array.length === 0){
+    return 0 //Retorna 0 se o array de alunos estiver vazio
+  }
+  let somaNotas = 0
 
+  array.forEach((aluno) => {
+    somaNotas += Number(aluno.nota)
+  })
+  const media = somaNotas / array.length
+  return media
 }
 
 ////////////////////////////////////////////////////////////////////////
